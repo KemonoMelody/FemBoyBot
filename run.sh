87833c8ff87c4b275c1ad295789f41898d8affc2
Bruno Heredia <111712756+Bruno5430@users.noreply.github.com>
2025-10-16 18:20:15 -0300

    Actualizar script para usar Pipenv

--- run.sh
+++ run.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-# Script para activar entorno virtual y ejecutar main.py
 
-# Ruta al entorno virtual
-VENV_DIR="venv"
-
-# Verifica si el entorno virtual existe
-if [ ! -d "$VENV_DIR" ]; then
-    echo "❌ No se encontró el entorno virtual en: $VENV_DIR"
-    exit 1
-fi
-
-# Activar el entorno virtual
-source "$VENV_DIR/bin/activate"
-
-# Ejecutar el archivo Python
-python bot.py
-
-# Desactivar el entorno virtual (opcional, porque se hace automáticamente al terminar el script)
-# deactivate
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+pipenv run python bot.py
